4df9dd0a2504de8e68c0c545248d79caa5c47b3a
Pal-cloud <paloma.gom.sal@gmail.com>
2025-12-18 13:37:26 +0100

    add libro_schema

--- models/libro_model.py
+++ models/libro_model.py
@@ -1,8 +1,8 @@
 from sqlalchemy import Column, Integer, String
 from .database import Base
 
-class Item(Base):
-    __tablename__ = "items"
+class Libro(Base):
+    __tablename__ = "libros"
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
--- schemas/libros_schema.py
+++ schemas/libros_schema.py
@@ -1,1 +1,18 @@
-# Libro schema definition
+from pydantic import BaseModel
+
+# Hereda todo de ItemBase.
+# Se usa cuando el usuario envía datos para crear un item.
+# “pass” significa que no añadimos nada nuevo, solo usamos lo que está en ItemBase.
+
+class ItemBase(BaseModel):
+    title: str
+    description: str = None
+
+class ItemCreate(LibroBase):
+    pass
+
+class Item(LibroBase): # Hereda de ItemBase (title y description) y añade el id que ya existe en la base de datos.
+    id: int
+
+    class Config:
+        orm_mode = True # permite que Pydantic lea directamente objetos de SQLAlchemy como si fueran diccionarios, para enviarlos en respuestas JSON.
